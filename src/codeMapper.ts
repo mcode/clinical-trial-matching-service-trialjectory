d79766f1a682128c12463750e7802e6ff17a4391
Robi Scalfani <rscalfani@mitre.org>
2021-08-05 16:48:44 -0400

    Lint Fixes

--- src/codeMapper.ts
+++ src/codeMapper.ts
@@ -2,7 +2,7 @@ import { Coding } from "./mcode";
 
 export class CodeMapper {
   // Map<Profile -> Map<System -> List<Codes>>>
-  code_map: Map<String, Map<String, String[]>>;
+  code_map: Map<string, Map<string, string[]>>;
 
   /**
    * Constructor
@@ -10,19 +10,17 @@ export class CodeMapper {
    */
   constructor(code_mapping_file: any) {
     this.code_map = CodeMapper.convertJsonToMap(code_mapping_file);
-    console.log(this.code_map);
-    console.log(JSON.stringify(this.code_map));
   }
 
   static convertJsonToMap(obj: {
     [key: string]: ProfileSystemCodes;
-  }): Map<String, Map<String, String[]>> {
-    const profile_map = new Map<String, Map<String, String[]>>();
+  }): Map<string, Map<string, string[]>> {
+    const profile_map = new Map<string, Map<string, string[]>>();
     for (const profile_key of Object.keys(obj)) {
-      const code_map = new Map<String, String[]>();
+      const code_map = new Map<string, string[]>();
       for (const system_key of Object.keys(obj[profile_key])) {
         const codes = obj[profile_key][system_key];
-        const code_strings: String[] = codes.map(function (code) {
+        const code_strings: string[] = codes.map(function (code) {
           return code.code;
         });
         code_map.set(system_key, code_strings);
@@ -39,13 +37,10 @@ export class CodeMapper {
   codeIsInMapping(coding: Coding, ...profiles: string[]): boolean {
     const system = CodeMapper.normalizeCodeSystem(coding.system);
     for (const profile of profiles) {
-      const code_profiles: Map<String, String[]> = this.code_map.get(profile); // Pull the codes for the profile
-      console.log(profile)
-      console.log("Stringified: " + JSON.stringify(this.code_map));
-      console.log("Str8 up: " + (this.code_map));
-      const codes_to_check: String[] = code_profiles.get(system);
+      const code_profiles: Map<string, string[]> = this.code_map.get(profile); // Pull the codes for the profile
+      const codes_to_check: string[] = code_profiles.get(system);
       if(codes_to_check == undefined){
-        throw "Profile \'" + profile + "\' does not exist in the given profile mappings."
+        throw "Profile '" + profile + "' does not exist in the given profile mappings."
       }
       if (
         codes_to_check.includes(coding.code) ||
@@ -98,12 +93,11 @@ export class CodeMapper {
     } else if (lowerCaseCodeSystem.includes("hl7")) {
       return "HL7";
     } else {
-      console.log("Profile codes do not support code system: " + codeSystem);
-      return "";
+      throw ("Profile codes do not support code system: " + codeSystem);
     }
   }
 }
 
 interface ProfileSystemCodes {
-  [system: string]: { code: String }[];
+  [system: string]: { code: string }[];
 }
