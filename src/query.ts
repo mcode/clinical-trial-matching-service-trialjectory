b79b00fde688c5c56fd99e80456c947e84e812cc
Christine Duong <cqduong@mitre.org>
2023-11-29 16:03:47 -0500

    Grab more results with null distance and remove closest facility mapping

--- src/query.ts
+++ src/query.ts
@@ -256,7 +256,7 @@ export class APIQuery {
     }
 
     // TODO: For now, overwrite the travelRadius to be null regardless!! TJ can't handle distance yet.
-    // this.travelRadius = null;
+    this.travelRadius = null;
 
     const mappingLogic = new TrialjectoryMappingLogic(patientBundle);
     console.log(mappingLogic);
